b2d0496cd67e3fef3f718e837a86f5e5017339d8
Todd Brown <36738690+toddcbrown@users.noreply.github.com>
2022-03-06 01:38:14 +0000

    Now works with average time and excludes 0

--- convert.py
+++ convert.py
@@ -1,3 +1,12 @@
+import pandas as pd
+import numpy as np
+
+def sec_to_min(x):
+  """
+  converts integer seconds to timedelta format HH:MM:SS
+  """
+  return str(pd.to_timedelta(x,'s')).split(' ')[2].split('.')[0]
+  
 def conv_to_sec(x):
   """
   Converts string time to integer seconds
@@ -12,20 +21,9 @@ def conv_to_sec(x):
     sec = int(j[0])*60*60 + int(j[1])*60 + round(float(j[2]))
     return int(sec)
 
-
-def gender(x,categories):
+def mean_tt_time(x):
   """
-  x = list of categories
-  categories = list of girl categories
+  x: a list of string times 
+  this turns a list of string times into integer seconds and back to MM:SS
   """
-  gend = list()
-  for i in x:
-    if i in categories:
-      gend.append('F')
-    else:
-      gend.append('M')
-  return gend
-  
-def sec_to_min(x):
-  import pandas as pd
-  return str(pd.to_timedelta(x,'s')).split(' ')[2].split('.')[0]
+  return sec_to_min(np.mean([(conv_to_sec(i)) for i in x]))
--- email_.py
+++ email_.py
@@ -1,34 +1,40 @@
+import yagmail
+import convert
+import numpy as np
+import pandas as pd
 def email(df,lastTT,compare_TT=None):
   """
   **Must use following columns in df
   ['Name','Last','email_rider','email_parent','Cat', 'May_21','June_21','July_21','Aug_21','May_22','June_22','July_22','Aug_22']
   """
-  sender = 'boor3657@gmail.com'
-  password = 'hnfe wank biyc cwul'
+  df = df.replace('',int(0))
+  sender = 'skyridge.mtb.tt.results@gmail.com'
+  password = 'uqrs dxjs vpos ixcw'
   yag = yagmail.SMTP(user=sender,password=password)
-  todays_mean = convert.sec_to_min(
-    np.mean(
-      lastTT.apply(
-        convert.conv_to_sec.loc[lastTT.apply(conv_to_sec) > 0])))
+  todays_mean = convert.mean_tt_time([i for i in lastTT if i != 0])
   for i in df.index:
     Name = df['Name'].iloc[i]
     recepient_rider = df['email_parent'].iloc[i]
     recepient_parent = df['email_parent'].iloc[i]
     subject = "Skyridge Junior Devo MTB Team | %s TT Results"%(Name)
-    contents = """%s's TT results are
+    Name = df['Name'].iloc[i]
+    recepient_rider = df['email_rider'].iloc[i]
+    recepient_parent = df['email_parent'].iloc[i]
+    subject = "Skyridge JD MTB Team | %s TT Results"%(Name)
+    contents = """%s's TT results are:\n-----------
     MAY 2021: %s
     JUNE 2021: %s
     JULY 2021: %s
     AUG 2021: %s
-    
+
     MAY 2022: %s
     JUNE 2022: %s
     JULY 2022: %s
     AUG 2022: %s
-    
+
     If you believe this is incorrect please text me at (801)669-2560.
-    These results are weighed heavily on deciding practice groups.
-    If you feel the need to have a makeup then you can talk to a coach and use Strava.
+    These results are weighed heavily on deciding practice groups. \n\n
+    Time trials can be made up using <a href="https://www.strava.com/segments/15283565"> this Strava segment</a>. Please notify a coach if you decide to do this. Also we will add 4 percent to the time because it does not cover the full loop.
     
     --------------
     The following is for your FYI, you can choose wether to share it with your child:
@@ -46,5 +52,35 @@ def email(df,lastTT,compare_TT=None):
                    df['Aug_22'].iloc[i],
                    todays_mean)
     
-    yag.send(to=recepient_parent,subject=subject,contents=contents)
-    print('%s email sent'% recepient_rider)
+    contents_rider = """Your TT results are:\n-----------
+    MAY 2021: %s
+    JUNE 2021: %s
+    JULY 2021: %s
+    AUG 2021: %s
+
+    MAY 2022: %s
+    JUNE 2022: %s
+    JULY 2022: %s
+    AUG 2022: %s
+
+     
+    Thanks!
+    Coach Todd"""%(
+                   df['May_21'].iloc[i],
+                   df['June_21'].iloc[i],
+                   df['July_21'].iloc[i],
+                   df['Aug_21'].iloc[i],
+                   df['May_22'].iloc[i],
+                   df['June_22'].iloc[i],
+                   df['July_22'].iloc[i],
+                   df['Aug_22'].iloc[i])
+    
+    try:
+      yag.send(to=recepient_rider,subject=subject,contents=contents_rider)
+    except:
+      print(Name + ' rider email failed')
+    try:
+      yag.send(to=recepient_parent,subject=subject,contents=contents)
+    except:
+      print(Name + ' parent email failed')
+    print('.')
